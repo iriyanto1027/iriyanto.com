89bfb328f6f9d41ef870046292ecc6263c020558
Iriyanto <iriyanto1027@gmail.com>
2025-02-20 18:22:36 +0700

    build: remove base path

--- svelte.config.js
+++ svelte.config.js
@@ -21,10 +21,6 @@ const config = {
 			precompress: false,
 			strict: true
 		}),
-
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
 	}
 };
 export default config;
