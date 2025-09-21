eac33b8b7ba0ea6cc6a5e763a0a9a8054ff45a67
Iriyanto <iriyanto1027@gmail.com>
2025-09-21 20:33:32 +0700

    feat: google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import Footer from '$lib/components/Footer.svelte';
+	import GoogleAnalytics from '$lib/components/GoogleAnalytics.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
+
+<GoogleAnalytics />
 
 <div class="grid h-screen grid-rows-[auto_1fr_auto]">
 	<!-- Header -->
